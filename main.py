bc355c62c16f338b61c543c4897ea5a9a0e69c7b
m10610 <26676096+m10610@users.noreply.github.com>
2023-04-19 15:23:13 -0500

    Initial

---
+++ main.py
@@ -0,0 +1,20 @@
+import hashlib as hl
+def signUp():
+    print("Create a New Account")
+    user = input("Enter a Username: ")
+    # TODO Filter Username
+    passwd = input("Enter a Password: ")
+    confirm_passwd = input("Confirm your Password: ")
+    if passwd == confirm_passwd:
+        encode = passwd.encode()
+        hash0 = hl.md5(encode).hexdigest()
+        with open("users.txt", "w") as f:
+            f.write(user + '\n')
+            f.write(hash0)
+        f.close()
+        print("Registration Successful")
+    else:
+        print("Passwords do not match try again")
+        signUp()
+
+signUp()
